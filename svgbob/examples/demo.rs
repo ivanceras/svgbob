edfd78e8399f0a25a39946e12236955699128897
Jovansonlee Cesar <ivanceras@gmail.com>
2017-04-02 05:35:28 +0800

    somehow examples got lost, restoring

---
+++ svgbob/examples/demo.rs
@@ -0,0 +1,118 @@
+extern crate svgbob;
+extern crate svg;
+
+use svgbob::Grid;
+use svgbob::Settings;
+
+
+fn main() {
+    let file = "examples/demo.svg";
+    let g = Grid::from_str(get_arg());
+    let svg = g.get_svg(&Settings::compact());
+    svg::save(file, &svg).unwrap();
+    println!("Saved to {}",file);
+}
+
+fn get_arg() -> &'static str{
+
+let arg = r#"
++------+   +-----+   +-----+   +-----+
+|      |   |     |   |     |   |     |
+| Foo  +-->| Bar +---+ Baz |<--+ Moo |
+|      |   |     |   |     |   |     |
++------+   +-----+   +--+--+   +-----+
+              ^         |
+              |         V
+.-------------+-----------------------.
+| Hello here and there and everywhere |
+'-------------------------------------'
+                        ____________
+   .--------------.     \           \
+  / a == b         \     \           \     __________
+ (    &&            )     ) process   )    \         \
+  \ 'string' ne '' /     /           /     / process /
+   '--------------'     /___________/     /_________/
+    __________________
+    \_________________\
+     \                 \
+      . another process .
+     /_________________/
+    /_________________/
+  User code  ^               ^ OS code
+              \             /
+               \        .--'
+                \      /
+  User code  <--- Mode ----> OS code
+                /      \
+            .--'        \___
+           /                \
+          v                  v 
+       User code            OS code
+             .---.  .---. .---.  .---.    .---.  .---.
+    OS API   '---'  '---' '---'  '---'    '---'  '---'
+               |      |     |      |        |      |
+               v      v     |      v        |      v
+             .------------. | .-----------. |  .-----.
+             | Filesystem | | | Scheduler | |  | MMU |
+             '------------' | '-----------' |  '-----'
+                    |       |      |        |
+                    v       |      |        v
+                 .----.     |      |    .---------.
+                 | IO |<----'      |    | Network |
+                 '----'            |    '---------'
+                    |              |         |
+                    v              v         v
+             .---------------------------------------.
+             |                  HAL                  |
+             '---------------------------------------'
+             
+   ____[]
+  | ___ |
+  ||   ||  device
+  ||___||  loads
+  | ooo |----------------------------------------------------------.
+  | ooo |    |                          |                          |
+  | ooo |    |                          |                          |
+  '-----'    |                          |                          |
+             |                          |                          |
+             v                          v                          v
+   .-------------------.  .---------------------------.  .-------------------.
+   | Loadable module C |  |     Loadable module A     |  | Loadable module B |
+   '-------------------'  |---------------------------|  |   (instrumented)  |
+             |            |         .-----.           |  '-------------------'
+             '------------+-------->| A.o |           |             |
+                 calls    |         '-----'           |             |
+                          |    .------------------.   |             |
+                          |   / A.instrumented.o /<---+-------------'
+                          |  '------------------'     |    calls
+                          '---------------------------'   
+        .--------------.
+         \              \
+          '--------------'
+                                        .--> Base::Class::Derived_A
+                                       /
+                                      .----> Base::Class::Derived_B    
+      Something -------.             /         \
+                        \           /           .---> Base::Class::Derived
+      Something::else    \         /             \
+            \             \       /               '--> Base::Class::Derived
+             \             \     /
+              \             \   .-----------> Base::Class::Derived_C 
+               \             \ /
+                '------ Base::Class
+                       /  \ \ \
+                      '    \ \ \  
+                      |     \ \ \
+                      .      \ \ '--- The::Latest
+                     /|       \ \      \
+ With::Some::fantasy  '        \ \      '---- The::Latest::Greatest
+                     /|         \ \
+         More::Stuff  '          \ '- I::Am::Running::Out::Of::Ideas
+                     /|           \
+         More::Stuff  '            \
+                     /              '--- Last::One
+       More::Stuff  V 
+"#;
+
+arg
+}
