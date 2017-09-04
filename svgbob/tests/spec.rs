9df652975c1104a0ab2315b12198b31667280d94
Jovansonlee Cesar <ivanceras@gmail.com>
2017-09-04 16:32:09 +0800

    Fix tests

--- svgbob/tests/spec.rs
+++ svgbob/tests/spec.rs
@@ -10,9 +10,7 @@ fn test_dash_alone(){
     println!("grid: {:?}", g);
     let result = g.get_svg_nodes_only();
     println!("grid: {:?}", g.to_string());
-    let expected = r#"<text x="1" y="12">
--
-</text>"#;
+    let expected = "<line x1=\"0\" x2=\"8\" y1=\"8\" y2=\"8\"/><line x1=\"0\" x2=\"8\" y1=\"8\" y2=\"8\"/>";
     assert_eq!(result, expected);
 }
 #[test]
@@ -32,7 +30,7 @@ fn test_vert_alone(){
 fn test_dash_line2(){
     use svgbob::{Grid,Settings};
 
-    let g = Grid::from_str("--", &Settings::separate_lines());
+    let g = Grid::from_str("--", &Settings::compact());
     let result = g.get_svg_nodes_only();
     let expected = r#"<line x1="0" x2="16" y1="8" y2="8"/>"#;
     assert_eq!(result, expected);
