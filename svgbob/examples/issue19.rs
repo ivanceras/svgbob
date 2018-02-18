205d146b3338f11d7e765cfa7306e4c03d9bf3a5
Jovansonlee Cesar <ivanceras@gmail.com>
2018-02-19 02:40:58 +0800

    Added example to issue19

---
+++ svgbob/examples/issue19.rs
@@ -0,0 +1,28 @@
+
+extern crate svgbob;
+extern crate svg;
+
+use svgbob::Grid;
+use svgbob::Settings;
+
+
+fn main() {
+    let file = "examples/issue19.svg";
+    let arg = r#"
+
+Issue 19 demo test  -------
+
+    +-----------+---+
+    | "a.to(b)" | c |
+    +-----------+---+
+
+    +-----------+---+
+    | "a.to(b)" | c |""
+    +-----------+---+
+    "#;
+    let g = Grid::from_str(arg, &Settings::compact());
+    let svg = g.get_svg();
+    svg::save(file, &svg).unwrap();
+    println!("Saved to {}",file);
+}
+
