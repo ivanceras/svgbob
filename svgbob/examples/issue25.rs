977d3d9243f80f2a03559e819897d0615696e1e5
Jovansonlee Cesar <ivanceras@gmail.com>
2018-05-22 21:17:17 +0800

    Add example for displaying issue#25

---
+++ svgbob/examples/issue25.rs
@@ -0,0 +1,25 @@
+extern crate svg;
+extern crate svgbob;
+
+use svgbob::Grid;
+use svgbob::Settings;
+
+fn main() {
+    let file = "examples/issue25.svg";
+    let arg = r#"
+------------.                
+             \ .-------------
+              X              
+             / '------------>
+------------'                
+
+  --. .---
+     X
+  --' '--
+
+    "#;
+    let g = Grid::from_str(arg, &Settings::compact());
+    let svg = g.get_svg();
+    svg::save(file, &svg).unwrap();
+    println!("Saved to {}", file);
+}
