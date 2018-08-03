85b7202ad560b2877c12011e7654ea1cfb249743
Jovansonlee Cesar <ivanceras@gmail.com>
2018-08-03 13:22:42 +0800

    regen

---
+++ svgbob/examples/long.rs
@@ -0,0 +1,14 @@
+extern crate svg;
+extern crate svgbob;
+
+use svgbob::Grid;
+use svgbob::Settings;
+
+fn main() {
+    let file = "examples/long.svg";
+    let arg = include_str!("long.bob");
+    let g = Grid::from_str(arg, &Settings::compact());
+    let svg = g.get_svg();
+    svg::save(file, &svg).unwrap();
+    println!("Saved to {}", file);
+}
