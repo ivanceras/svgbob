9cea9cae45e46e04cfad830a9a081e1444e0cab9
Jovansonlee Cesar <ivanceras@gmail.com>
2019-10-20 11:13:36 +0800

    Add timing in demo example

--- svgbob/examples/demo.rs
+++ svgbob/examples/demo.rs
@@ -1,14 +1,17 @@
 extern crate svg;
 extern crate svgbob;
 
+use std::time::Instant;
 use svgbob::Grid;
 use svgbob::Settings;
 
 fn main() {
     let file = "examples/demo.svg";
+    let t1 = Instant::now();
     let arg = include_str!("demo.bob");
     let g = Grid::from_str(arg, &Settings::default());
     let svg = g.get_svg();
+    println!("took {}ms", t1.elapsed().as_millis());
     svg::save(file, &svg).unwrap();
     println!("Saved to {}", file);
 }
